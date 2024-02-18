17f7cf346cb213476f9e711253d4b16381c863b4
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-18 18:21:45 +0530

    feat: add commands with keep open

--- plugin/termim.lua
+++ plugin/termim.lua
@@ -4,13 +4,6 @@ end
 
 vim.g.loaded_termim = true
 
-vim.api.nvim_create_autocmd({ 'TermClose' }, {
-    group = vim.api.nvim_create_augroup('termim_auto_close', { clear = true }),
-    callback = function()
-        vim.cmd('quit')
-    end,
-})
-
 vim.api.nvim_create_autocmd({ 'TermOpen' }, {
     group = vim.api.nvim_create_augroup('termim_open', { clear = true }),
     callback = function(event)
@@ -21,16 +14,28 @@ vim.api.nvim_create_autocmd({ 'TermOpen' }, {
     end,
 })
 
-vim.api.nvim_create_user_command('Term', function(input)
+vim.api.nvim_create_user_command('Fterm', function(input)
     require("termim").open(input.args)
+end, { nargs = '*' })
+
+vim.api.nvim_create_user_command('Fterm', function(input)
+    require("termim").open(input.args, nil, true)
 end, { nargs = '*' })
 
 vim.api.nvim_create_user_command('Sterm', function(input)
     require("termim").open(input.args, 'split')
 end, { nargs = '*' })
 
+vim.api.nvim_create_user_command('STerm', function(input)
+    require("termim").open(input.args, 'split', true)
+end, { nargs = '*' })
+
 vim.api.nvim_create_user_command('Vterm', function(input)
     require("termim").open(input.args, 'vsplit')
+end, { nargs = '*' })
+
+vim.api.nvim_create_user_command('VTerm', function(input)
+    require("termim").open(input.args, 'vsplit', true)
 end, { nargs = '*' })
 
 vim.keymap.set('t', 'JJ', '<C-\\><C-n>', { buffer = true })
